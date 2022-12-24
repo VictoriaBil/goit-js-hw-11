2c7157cafc2b12b2f1fd7abf779bc6fc98e3da94
Victoria Bilauchuk <victoriya.bilauchuk@gmail.com>
2022-12-24 18:09:29 +0200

    update render

--- src/index.js
+++ src/index.js
@@ -22,45 +22,10 @@ function onSearch(e) {
     .catch(err => console.log(err));
 }
 
-// function renderGallery(data) {
-//   const markupGallery = data
-//     .map(
-//       ({
-//         webformatURL,
-//         largeImageURL,
-//         tags,
-//         likes,
-//         views,
-//         comments,
-//         downloads,
-//       }) =>
-//         `<a class="gallery__link" href="${largeImageURL}">
-//             <img class="gallery__image" src="${webformatURL}" alt="${tags}" loading="lazy" />
-//             <div class="gallery__info">
-//               <p class="gallery__info--item">
-//                 <b>Likes</b>${likes}
-//               </p>
-//               <p class="gallery__info--item">
-//                 <b>Views</b>${views}
-//               </p>
-//               <p class="gallery__info--item">
-//                 <b>Comments</b>${comments}
-//               </p>
-//               <p class="gallery__info--item">
-//                 <b>Downloads</b>${downloads}
-//               </p>
-//             </div>
-//           </a>`
-//     )
-//     .join('');
-
-//   gallery.insertAdjacentHTML('beforeend', markupGallery);
-// }
-
-function renderGallery(images) {
-  const markup = images
-    .map(image => {
-      const {
+function renderGallery({ data }) {
+  const markupGallery = data.hits
+    .map(
+      ({
         webformatURL,
         largeImageURL,
         tags,
@@ -68,24 +33,28 @@ function renderGallery(images) {
         views,
         comments,
         downloads,
-      } = image;
-      return `
-        <a class="gallery__link" href="${largeImageURL}">
-          <div class="gallery-item" id="${id}">
-            <img class="gallery-item__img" src="${webformatURL}" alt="${tags}" loading="lazy" />
-            <div class="info">
-              <p class="info-item"><b>Likes</b>${likes}</p>
-              <p class="info-item"><b>Views</b>${views}</p>
-              <p class="info-item"><b>Comments</b>${comments}</p>
-              <p class="info-item"><b>Downloads</b>${downloads}</p>
+      }) =>
+        `<a class="gallery__link" href="${largeImageURL}">
+            <img class="gallery__image" src="${webformatURL}" alt="${tags}" loading="lazy" />
+            <div class="gallery__info">
+              <p class="gallery__info--item">
+                <b>Likes</b>${likes}
+              </p>
+              <p class="gallery__info--item">
+                <b>Views</b>${views}
+              </p>
+              <p class="gallery__info--item">
+                <b>Comments</b>${comments}
+              </p>
+              <p class="gallery__info--item">
+                <b>Downloads</b>${downloads}
+              </p>
             </div>
-          </div>
-        </a>
-      `;
-    })
+          </a>`
+    )
     .join('');
 
-  gallery.insertAdjacentHTML('beforeend', markup);
+  gallery.insertAdjacentHTML('beforeend', markupGallery);
 }
 
 function cleanGallery() {
