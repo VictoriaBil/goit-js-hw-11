8db5c10818b296e0872baef9b88275fd7fdab923
Victoria Bilauchuk <victoriya.bilauchuk@gmail.com>
2022-12-24 16:49:57 +0200

    fixes

--- src/fetch.js
+++ src/fetch.js
@@ -3,12 +3,9 @@ export { fetchImages };
 
 BASE_URL = 'https://pixabay.com/api/';
 const KEY = '32269286-83e5915044612836799c36253';
-let query = '';
-let page = 1;
-const perPage = 40;
 
-async function fetchImages(query, page, perPage) {
+async function fetchImages(query) {
   return await axios.get(
-    `${BASE_URL}?key=${KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`
+    `${BASE_URL}?key=${KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true`
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { fetchImages } from './fetch';
-import { renderGallery, cleanGallery } from './renderGallery';
+// import { renderGallery, cleanGallery } from './renderGallery';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import SimpleLightbox from 'simplelightbox';
 
 const form = document.querySelector('#search-form');
 const gallery = document.querySelector('.gallery');
@@ -9,9 +10,69 @@ form.addEventListener('submit', onSearch);
 
 function onSearch(e) {
   e.preventDefault();
+  cleanGallery();
+  // let query = '';
+  // let page = 1;
+  // const perPage = 40;
 
   query = e.currentTarget.searchQuery.value.trim();
 
-  fetchImages();
-  renderGallery();
+  fetchImages(query)
+    .then(renderGallery)
+    .catch(err => console.log(err));
 }
+
+function renderGallery(data) {
+  const markupGallery = data
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) =>
+        `<a class="gallery__link" href="${largeImageURL}">
+            <img class="gallery__image" src="${webformatURL}" alt="${tags}" loading="lazy" />
+            <div class="gallery__info">
+              <p class="gallery__info--item">
+                <b>Likes</b>${likes}
+              </p>
+              <p class="gallery__info--item">
+                <b>Views</b>${views}
+              </p>
+              <p class="gallery__info--item">
+                <b>Comments</b>${comments}
+              </p>
+              <p class="gallery__info--item">
+                <b>Downloads</b>${downloads}
+              </p>
+            </div>
+          </a>`
+    )
+    .join('');
+
+  gallery.insertAdjacentHTML('beforeend', markupGallery);
+}
+
+function cleanGallery() {
+  gallery.innerHTML = '';
+}
+
+// function alertNoEmptySearch() {
+//   Notify.failure(
+//     'The search string cannot be empty. Please specify your search query.'
+//   );
+// }
+
+// function alertNoImagesFound() {
+//   Notify.failure(
+//     'Sorry, there are no images matching your search query. Please try again.'
+//   );
+// }
+
+// function alertEndOfSearch() {
+//   Notify.failure("We're sorry, but you've reached the end of search results.");
+// }
